49e54ca97654c51bde6df2fac80e67e20cbd23d4
Pavel <3abodskoy123@gmail.com>
2023-07-09 12:41:29 +0300

    dit integratoin

---
+++ src/main/java/Main.java
@@ -0,0 +1,26 @@
+
+import jm.task.core.jdbc.service.UserService;
+import jm.task.core.jdbc.service.UserServiceImpl;
+
+
+public class Main {
+    private final static UserService userService = new UserServiceImpl();
+
+    public static void main(String[] args) {
+        userService.createUsersTable();
+
+        userService.saveUser("Sanya", "Zeleniy", (byte) 32);
+        userService.saveUser("Oleg", "Cherniy", (byte) 23);
+        userService.saveUser("Misha", "Rozoviy", (byte) 45);
+        userService.saveUser("Alex", "Belyi", (byte) 65);
+
+        userService.removeUserById(2);
+
+        userService.getAllUsers();
+
+//        userService.cleanUsersTable();
+//
+//        userService.dropUsersTable();
+    }
+}
+
